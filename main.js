88cb168d1a28340c5d2bd4ba4322ecbc56aaa32b
Grzegorz Sawinski <sawinskigrzesiu@gmail.com>
2023-03-23 13:24:59 +0100

    React init

---
+++ main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+
+function HomePage(){
+    return (
+        <div>
+            <h1>BookShelf HomePage</h1>
+        </div>
+    );
+};
+
+ReactDOM.render(<HomePage />, document.getElementById('root'));

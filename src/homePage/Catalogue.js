0bd26a72d9479fa9ba5330231470e7576de6a5c5
Grzegorz Sawinski <sawinskigrzesiu@gmail.com>
2023-03-29 23:36:25 +0200

    Dodanie kafelek ksiazek z GoogleAPI

--- src/homePage/Catalogue.js
+++ src/homePage/Catalogue.js
@@ -24,6 +24,51 @@ function CatalogueNavbar(props){
     )
 }
 
+function BookCard(props){
+    const bookCardStyle = {backgroundColor: "white", width:"200px", height:"350px", padding:"10px",
+                           borderRadius:"12px",boxShadow: "0px 8px 16px 0px rgba(0,0,0,0.2)"}
+
+    const book = props.data
+
+    return(
+        <Container fluid className="d-flex flex-column bd-highlight mb-3" style={bookCardStyle}>
+            {book.imageLinks && <img src={book.imageLinks.thumbnail}></img>}
+            {book.title && <h1 style={{fontWeight:"bold", fontSize:"15px"}}>{book.title}</h1>}
+            {book.authors && <h1 style={{fontSize:"12px"}}>{`${book.authors[0]}`}</h1>}
+        </Container>
+    )
+}
+
+function RandomBookPanel(props){
+    const [books, setBooks] = React.useState([])
+
+    const category = props.category
+
+    React.useEffect(() =>{
+        fetch("https://www.googleapis.com/books/v1/volumes?q=category:" + category)
+        .then(res => res.json())
+        .then((data) => {
+            const volumeInfo = data.items.map((book) =>{
+                return book.volumeInfo
+            })
+            setBooks(volumeInfo)
+        })
+    },[])
+
+    const BookCardList = books.map((book) =>{
+        return <BookCard data={book} />
+    })
+
+    return(
+        <Container>
+            <h1 style={{marginLeft:"40px"}}>{category}</h1>
+            <Container fluid className="d-flex flex-wrap" style={{padding:"15px"}}>
+                {BookCardList.slice(5)}
+            </Container>
+        </Container>
+    )
+}
+
 export function Main(){
     const mainStyle = {padding: "20px 20px 20px 20px"}
     const data = [{category: "Horror", id:1},
@@ -37,13 +82,14 @@ export function Main(){
                   {category: "etc3", id:9},
                   {category: "etc4", id:10},
                   {category: "etc5", id:11},
-                  {category: "etc6", id:12}] //KATEGORIE MOZNA DODAWAC 
+                  {category: "etc6", id:12}]
 
     return(
         <Container fluid className="d-flex justify-content-start" style={mainStyle}>
             <CatalogueNavbar data={data}/>
-            <Container>
-
+            <Container className="d-flex flex-column">
+                <RandomBookPanel category="Horrors"/>
+                <RandomBookPanel category="Fantasy"/>
             </Container>
         </Container>
     )

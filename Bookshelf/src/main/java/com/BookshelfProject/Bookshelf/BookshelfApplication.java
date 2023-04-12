b158fe34e9ca34e145e6d70949b334f59b25a35c
Kamil <pk44453@zut.edu.pl>
2023-04-12 17:16:22 +0200

    USER CRUD

--- Bookshelf/src/main/java/com/BookshelfProject/Bookshelf/BookshelfApplication.java
+++ Bookshelf/src/main/java/com/BookshelfProject/Bookshelf/BookshelfApplication.java
@@ -1,20 +1,12 @@
 package com.BookshelfProject.Bookshelf;
 
-import com.BookshelfProject.Bookshelf.User.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class BookshelfApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BookshelfApplication.class, args);
 	}
-
-
 }
+
---
+++ Bookshelf/src/main/java/com/BookshelfProject/Bookshelf/User/UserController.java
@@ -0,0 +1,44 @@
+package com.BookshelfProject.Bookshelf.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequestMapping({"/users"})
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return this.userService.getAllUsers();
+    }
+
+    @GetMapping({"/{id}"})
+    public User getUser(@PathVariable Long id) {
+        return this.userService.getById(id);
+    }
+
+    @PostMapping
+    public User addUser(@RequestBody User user) {
+        return this.userService.addUser(user);
+    }
+
+    @PutMapping({"/{id}"})
+    public User updateUser(@PathVariable Long id, @RequestBody User userDetails) {
+        return this.userService.updateUser(id, userDetails);
+    }
+
+    @DeleteMapping({"/{id}"})
+    public void deleteUser(@PathVariable Long id) {
+        this.userService.delete(id);
+    }
+}

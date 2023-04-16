44925f4eb8102c50dcbc9518a147e1b3140e60c5
Grzegorz Sawinski <sawinskigrzesiu@gmail.com>
2023-04-16 15:46:53 +0200

    dodanie filtrów

--- Mobile/src/MyBooks/Categories.js
+++ Mobile/src/MyBooks/Categories.js
@@ -2,11 +2,30 @@ import React from 'react'
 import { View, Text, StyleSheet} from 'react-native'
 import { FlatList, NativeViewGestureHandler, ScrollView } from 'react-native-gesture-handler'
 import BottomSheet from '../AddBook/BottomSheet'
-import { IconButton,Chip, Button } from 'react-native-paper'
-import CheckBox from './CheckBox'
+import { IconButton, Chip, Button, Divider } from 'react-native-paper'
+import CategoryCheckBox from './CategoryCheckBox'
 
 export default function Categories() {
   const refBottomSheet = React.useRef()
+  
+  const fetchedData =[{title: "asdasd", id: 14},
+                      {title: "asdasd", id: 1},
+                      {title: "asdasd", id: 2},
+                      {title: "asdasd", id: 3},
+                      {title: "asdasd", id: 4},
+                      {title: "asdasd", id: 5},
+                      {title: "asdasd", id: 6},
+                      {title: "asdasd", id: 7},
+                      {title: "asdasd", id: 8},
+                      {title: "asdasd", id: 9},
+                      {title: "asdasd", id: 10},
+                      {title: "asdasd", id: 11},
+                      {title: "asdasd", id: 12},
+                      {title: "asdasd", id: 13}]
+  const editedData = fetchedData.map(item => ({...item, isChecked: false}))
+
+  const [data, setData] = React.useState(editedData)
+
 
   function onHandlePress(){
     const isActive = refBottomSheet?.current?.isActive()
@@ -14,31 +33,29 @@ export default function Categories() {
     refBottomSheet?.current?.setIsVisible(true);
   }
 
-  const data =[{title: "asdasd", id: 14},
-  {title: "asdasd", id: 1},
-  {title: "asdasd", id: 2},
-  {title: "asdasd", id: 3},
-  {title: "asdasd", id: 4},
-  {title: "asdasd", id: 5},
-  {title: "asdasd", id: 6},
-  {title: "asdasd", id: 7},
-  {title: "asdasd", id: 8},
-  {title: "asdasd", id: 9},
-  {title: "asdasd", id: 10},
-  {title: "asdasd", id: 11},
-  {title: "asdasd", id: 12},
-  {title: "asdasd", id: 13}]
-
-  const renderCheckbox = ({item}) => {
-    return <CheckBox title={item.title} />
+  const handleCheck = (id) => {
+    const updatedData = data.map(item => {
+      if (item.id === id) {
+        return {...item, isChecked: !item.isChecked};
+      }
+      return item;
+    });
+    setData(updatedData);
   }
+
+  const renderCategoryCheckbox = ({item}) => {
+    return <CategoryCheckBox title={item.title} isChecked={item.isChecked} onChecked={() => handleCheck(item.id)} />
+  }
+
   return (
     <View style={styles.container}>
       <Button icon="filter" mode='contained-tonal' elevated={true} elevation={4} onPress={onHandlePress} style={styles.filterButton}>Filtruj</Button>
       <Chip icon="check" mode='flat' elevated={true} elevation={1} 
             style={styles.chipContainer} onClose={()=>console.log("close")} onPress={()=>console.log("pressed")}>Chip</Chip>
         <BottomSheet ref={refBottomSheet} scale={1.09}>
-            <FlatList data={data} renderItem={renderCheckbox} keyExtractor={item => item.id}/>
+          <Divider bold={true}/>
+          <Text style={styles.categoryTitle}>Kategorie</Text>
+          <FlatList data={data} renderItem={renderCategoryCheckbox} keyExtractor={item => item.id}/>
         </BottomSheet>
     </View>
   )
@@ -47,18 +64,25 @@ export default function Categories() {
 const styles = StyleSheet.create({
   container:{
     flex:1,
-    flexDirection:"row"
+    flexDirection:"column"
   },
   filterButton:{
     alignSelf:"flex-start",
     margin:10,
     borderWidth:0.5,
-    borderColor:"black"
+    borderColor:"black",
+    backgroundColor:"white"
   },
   chipContainer:{
-    marginTop:13.5,
+    marginLeft:10,
     alignSelf:"flex-start", 
     height:35, 
     width:"auto",
+    backgroundColor:"white"
+  },  
+  categoryTitle:{
+    fontSize:20,
+    fontWeight:"bold",
+    margin:10
   }
 })
--- Mobile/src/MyBooks/CategoryCheckBox.js
+++ Mobile/src/MyBooks/CategoryCheckBox.js
@@ -2,20 +2,25 @@ import React from "react"
 import { View,Text, StyleSheet } from "react-native"
 import { Checkbox } from "react-native-paper"
 
-export default function CheckBox(props){
-    const [checked, setChecked] = React.useState(false)
+export default function CategoryCheckBox(props){
+    const [checked, setChecked] = React.useState(props.isChecked)
+
+    function onCheck(){
+        setChecked(!checked)
+        props.onChecked()
+    }
 
     return(
         <View style={styles.container}>
-            <Checkbox status={checked ? 'checked':'unchecked'} onPress={()=> setChecked(!checked)} />
             <Text style={{marginTop:8}}>{props.title}</Text>
+            <Checkbox status={checked ? 'checked':'unchecked'} onPress={onCheck} />
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container:{
-        justifyContent:"flex-start",
+        justifyContent:"space-between",
         alignItems:"flex-start",
         flexDirection:"row",
         margin:10

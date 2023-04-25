aeb290539ca6834b116b6d474d31829d7c292ac8
Grzegorz Sawinski <sawinskigrzesiu@gmail.com>
2023-04-25 14:34:40 +0200

    deletion of unecessary cata

--- Bookshelf/src/main/java/com/BookshelfProject/Bookshelf/shelf/ShelfRepository.java
+++
@@ -1,9 +0,0 @@
-package com.BookshelfProject.Bookshelf.shelf;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ShelfRepository extends JpaRepository<Shelf, Long> {
-}
-

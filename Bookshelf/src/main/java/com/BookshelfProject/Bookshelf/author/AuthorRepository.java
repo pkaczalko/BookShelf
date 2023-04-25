eb70c36b08a9a0c236ff83a4cbf118ba699df8b0
Grzegorz Sawinski <sawinskigrzesiu@gmail.com>
2023-04-25 14:30:21 +0200

    deletion

--- Bookshelf/src/main/java/com/BookshelfProject/Bookshelf/author/AuthorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.BookshelfProject.Bookshelf.author;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AuthorRepository extends JpaRepository<Author, Long> {
-}
-
--- Bookshelf/src/main/java/com/BookshelfProject/Bookshelf/genre/GenreRepository.java
+++
@@ -1,9 +0,0 @@
-package com.BookshelfProject.Bookshelf.genre;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GenreRepository extends JpaRepository<Genre, Long> {
-}
-

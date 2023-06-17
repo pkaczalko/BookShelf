2e2c15124b531df674a9925699b32911d4bb2892
kp49285 <116281448+pkaczalko@users.noreply.github.com>
2023-06-17 20:29:14 +0200

    lol

--- Web/webpack.prod.js
+++ Web/webpack.prod.js
@@ -1,30 +1,30 @@
 const path = require('path');
 const common = require('./webpack.common');
 const merge = require('webpack-merge');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = merge.merge(common, {
-    mode: "production",
-    output: {
-        filename: 'main.[contenthash].js',
-        path: path.resolve(__dirname, "dist")
-    },
-    plugins: [
-        new MiniCssExtractPlugin({filename: "[name].[contenthash].css"}),
-        new CleanWebpackPlugin()
+module.exports = merge(common, {
+  mode: "production",
+  output: {
+    filename: 'main.[contenthash].js',
+    path: path.resolve(__dirname, "dist")
+  },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: "[name].[contenthash].css" }),
+    new CleanWebpackPlugin()
+  ],
+  module: {
+    rules: [
+      // {
+      //   test: /\.css$/i,
+      //   exclude: /(node_modules|bower_components)/,
+      //   use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      // }
+      {
+        test: /\.(scss|less|css)$/i,
+        use: ['style-loader', 'css-loader', "sass-loader"]
+      }
     ],
-    module: {
-        rules: [
-          // {
-          //   test: /\.css$/i,
-          //   exclude: /(node_modules|bower_components)/,
-          //   use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-          // }
-          {
-            test: /\.(scss|less|css)$/i,
-            use: ['style-loader', 'css-loader', "sass-loader"]
-          }
-        ],
-    } 
+  }
 });
